0347ef840c7f33b3041837f7b91962b1e88265a4
zuiran-github <zuyhan@163.com>
2021-05-20 10:26:29 +0800

    train

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,14 @@ from .views import *
 from django.conf.urls import url
 # from .allcompany import *
 from .zyh_flight import *
+from .zyh_train import *
 
 
 urlpatterns = [
 
 
-    url(r"allsearch$", allsearch, )  # 查询所有票的价格
+    url(r"allsearch$", allsearch, ),  # 查询所有机票的价格
+    url(r"getTraininfo", zyh_getTrainTicket, ),  # 查询所有火车票的价格
 
 
 
